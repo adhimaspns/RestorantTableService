0231c3fb165fa2b062a320398e6b7086d3fd1ff6
adhimaspns <adhimaspnsmjr1@gmail.com>
2022-02-02 18:41:14 +0700

    add UserSeeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user   = [
+
+            //! Kasir
+            [
+                'nama'          => "Adhimas Putra Nugraha Sugianto",
+                'alamat'        => "Dsn. Mlaten Ds. Mlaten RT 01 RW 02",
+                'jns_kelamin'   => "Laki-laki",
+                'no_telp'       => "08970898910",
+                'username'      => "kasir_adhimas",
+                'password'      => bcrypt('kasir123'),
+                'level'         => "Kasir"
+            ],
+
+            //! Customer
+            [
+                'nama'          => "Mustawi",
+                'alamat'        => "Dsn. Sambiroto Ds. Mlaten RT 03 RW 05",
+                'jns_kelamin'   => "Laki-laki",
+                'no_telp'       => "081615227898",
+                'username'      => "mustawi",
+                'password'      => bcrypt('mustawi123'),
+                'level'         => "Customer"
+            ] 
+        ];
+
+        //! Seeding Data
+        User::insert($user); 
+    }
+}
