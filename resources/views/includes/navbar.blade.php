f5afb60713c5bcad4916742d155bfe2786588232
adhimaspns <adhimaspnsmjr1@gmail.com>
2022-02-02 20:16:49 +0700

    add link user setting

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -7,19 +7,19 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
+                <li class="nav-item">
                     <a class="nav-link" href="#">Beranda <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Link</a>
                 </li>
-                <li class="nav-item dropdown">
+                <li class="nav-item dropdown @yield('setting')">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
                         Settings
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="#">Meja</a>
-                        <a class="dropdown-item" href="">Users</a>
+                        <a class="dropdown-item @yield('user')" href="{{ route('user.index') }}">Users</a>
                     </div>
                 </li>
                 <li class="nav-item">
