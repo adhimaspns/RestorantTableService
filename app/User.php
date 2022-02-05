a526c720137d57cff96ed73711da976a3ef4722f
adhimaspns <adhimaspnsmjr1@gmail.com>
2022-02-05 16:26:09 +0700

    add relationship booking

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Models\Booking;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -36,4 +37,12 @@ class User extends Authenticatable
     ];
 
     protected $primaryKey = 'id_user';
+
+    //! Relationship
+
+    //* Booking
+    public function booking()
+    {
+        return $this->hasMany(Booking::class);
+    } 
 }
